0c11a2ba8abd8cf568dacd78dabea571e619a6ac
Scott Baker <smbaker@gmail.com>
2019-10-12 15:06:52 -0700

    a more robust start_controller

--- start_controller.sh
+++ start_controller.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
+echo "start_controller.sh started" > /tmp/controller.script
+printf "%s" "waiting for ServerXY ..."
+while ! ping -c 1 -n -w 1 198.0.0.108 &> /dev/null
+do
+    printf "%c" "."
+done
 cd /home/pi
-nohup python controller.py -m &
+nohup python controller.py -m > /tmp/controller.out 2> /tmp/controller.err &
+echo "start_Ccontroller.sh exiting" >> /tmp/controller.script
